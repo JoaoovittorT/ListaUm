05f5e2c884a7f7617dafde5a1722d3f18bf80cac
JoaoovittorT <joao.vta20@gmail.com>
2024-05-15 11:43:11 -0300

    Create ContaCorrente.java

---
+++ ContaCorrente.java
@@ -0,0 +1,39 @@
+	public class ContaCorrente {
+	private int numeroConta;
+	private double saldo;
+		
+	public ContaCorrente(int novoNumeroConta,double novoSaldo ) {
+		    this.numeroConta = novoNumeroConta;
+		    this.saldo = novoSaldo;
+		       
+	}
+	
+		//GETT();
+		public int getNumeroConta() {
+			return numeroConta;
+			
+			}
+		public double getSaldo() {
+			return saldo;
+			}
+		
+		
+		//SET();
+		public void setSaldo(double novoSaldo) {
+		  this.saldo = novoSaldo;
+		  
+	  		}
+	  
+		  
+		 public void sacar(double valor) {
+			 this.saldo= saldo - valor;
+		  		}
+	  
+	  public void depositar(double valor) {
+		 this.saldo= saldo + valor;
+	  		}
+	  
+	  public String toString () {
+		  return "Número da conta:" + this.numeroConta + "\n saldo: R$ " + this.saldo; 
+	  }
+}
