7764241249b9deed1cc1360fbf2be2bcb5938785
JoaoovittorT <joao.vta20@gmail.com>
2024-05-15 11:05:04 -0300

    Create Calculadora.java

---
+++ Calculadora.java
@@ -0,0 +1,24 @@
+public class calculadora {
+
+	private int a;
+	private int b;
+
+	public int somar (int a, int b) {
+		return a + b;
+	}
+	
+
+	public int subtrair (int a, int b) {
+		return a - b;
+	}
+	
+	public int mutiplicar (int a, int b) {
+		return a * b;
+	}
+	
+	public int dividir (int a, int b) {
+		return a / b;
+	}
+			
+	
+}
