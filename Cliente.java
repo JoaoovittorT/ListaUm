78976406cf47e922b7018645903b60bc4d47b370
JoaoovittorT <joao.vta20@gmail.com>
2024-05-20 08:50:08 -0300

    Create Cliente.java

---
+++ Cliente.java
@@ -0,0 +1,26 @@
+public class Cliente {
+
+		String nome;
+		String cpf;
+		String dataNascimento;
+		
+		//construtor
+
+		
+		public void Pessoa (String novoNome) {
+			this.nome = novoNome;
+		}
+		
+		
+		
+		public String getNome(){
+			return nome;
+		}
+			
+
+		public void setNome(String novoNome) {
+			this.nome = novoNome;
+		}
+			
+}
+
