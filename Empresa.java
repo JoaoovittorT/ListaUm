3bc55d3bf9ed13e943d51979bfd5fd76548a2a8c
JoaoovittorT <joao.vta20@gmail.com>
2024-05-20 10:37:14 -0300

    Create Empresa.java

---
+++ Empresa.java
@@ -0,0 +1,29 @@
+public class Empresa {
+	
+	private String nome;
+	private String cnpj;
+	
+	public Empresa(String novoNome, String novoCnpj) {
+		this.nome = novoNome;
+		this.cnpj = novoCnpj;
+	}
+	
+//GET{};
+	
+	public String getNome() {
+		return nome;
+	}
+	public String getCnpj() {
+		return cnpj;
+	}
+	
+//SET{};
+	
+	public void setNome(String novoNome) {
+		this.nome = novoNome;
+	}
+	public void setCnpj(String novoCnpj) {
+		this.cnpj = novoCnpj;
+	}
+
+}
